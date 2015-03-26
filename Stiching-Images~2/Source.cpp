32c758476d79a28880f1598bcc72175b6645c154
Karan Jeet <karanjeet96@gmail.com>
2015-03-26 15:53:20 -0700

    Stiching openCV
    Stiching two images into one using open CV library

---
+++ Stiching-Images~2/Source.cpp
@@ -0,0 +1,18 @@
+#include<opencv\cv.h>
+#include <opencv2\core\core.hpp>
+#include <opencv2\features2d\features2d.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\nonfree\nonfree.hpp>
+#include <opencv2\calib3d\calib3d.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+void main(){
+
+
+
+
+}
